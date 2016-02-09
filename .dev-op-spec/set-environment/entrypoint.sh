5e99f9898d6ee2b0608fd4d775ae19d708de2a9d
Chris Dostert <chris.dostert@live.com>
2016-02-08 21:16:06 -0800

    cleanup

--- .dev-op-spec/set-environment/entrypoint.sh
+++ .dev-op-spec/set-environment/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 summon \
   -f .dev-op-spec/set-environment/env.yml \
-  tail -c1 @SUMMONENVFILE | read -r _ || echo > .dev-op-spec/.common/env
+  cat @SUMMONENVFILE > .dev-op-spec/.common/env
